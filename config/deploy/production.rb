4c06b2d4ee8090c90009113703577b273d476e31
Néstor Coppi <nestorcoppi@gmail.com>
2013-11-29 13:43:23 -0300

    Probando roles capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,7 @@
 set :stage, :production
 
-role :all, %w{mbm-datos.no-ip.org}
+role :web, 'mbm-datos.no-ip.org'
+role :app, 'mbm-datos.no-ip.org'
+role :db, 'mbm-datos.no-ip.org', primary: true
 
-server 'mbm-datos.no-ip.org', user: 'deployer', roles: :all
+server 'mbm-datos.no-ip.org', user: 'deployer', roles: %w{web app db}
